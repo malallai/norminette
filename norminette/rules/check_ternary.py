df6f9c92de05ed0d1ce4df1286e7b1565a2adbf8
Malo ALLAIN <malo.albx@gmail.com>
2025-06-06 14:23:42 +0200

    feat: allow ternary in define with -R CheckDefine (#560)

--- norminette/rules/check_ternary.py
+++ norminette/rules/check_ternary.py
@@ -7,6 +7,12 @@ class CheckTernary(Rule, Check):
         Ternaries are forbidden
         """
         for i in range(0, context.tkn_scope):
+            if (
+                context.check_token(i, "IDENTIFIER")
+                and context.peek_token(i).value == "define"
+                and context.preproc.skip_define
+            ):
+                return
             if context.check_token(i, "TERN_CONDITION") is True:
                 context.new_error("TERNARY_FBIDDEN", context.peek_token(i))
         return False, 0
